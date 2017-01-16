0949f9ab2e87357ebe80d54c0140c584ba3ae6d7
matheusota <matheusota@gmail.com>
2017-01-16 17:38:37 -0200

    comparing 2 points and pause button
    space is pause

--- attractor/sketch.js
+++ attractor/sketch.js
@@ -3,6 +3,10 @@ var x = 0.01;
 var y = 0;
 var z = 0;
 
+var x2 = 0.02;
+var y2 = 0;
+var z2 = 0;
+
 var a = 10;
 var b = 28;
 var c = 8.0 / 3.0;
@@ -10,11 +14,18 @@ var c = 8.0 / 3.0;
 var delta = 10;
 
 var points = [[x, y, z]];
+var points2 = [[x2, y2, z2]];
+
 var sens = 0.01;
 var it = 0;
 
+var h1 = 185;
+var h2 = 55;
+
 //matrix to apply the rotations
 var rotmatrix = math.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]]);
+
+var pause = false;
 
 //this will run only at the start
 function setup(){
@@ -35,6 +46,12 @@ function draw(){
       rotate3D(0, 1, 0, sens);
     else if(keyCode == RIGHT_ARROW)
       rotate3D(0, -1, 0, sens);
+    else if(keyCode == 32){
+      if(!pause)
+        pause = true;
+      else
+        pause = false;
+    }
   }
   
   //clearing all
@@ -43,7 +60,7 @@ function draw(){
   
   //add new points  
   //compute differentials(lorenz attractor)
-  if(it < 1000){
+  if(it < 10000 && !pause){
     var dt = 0.01;
     var dx = (a * (y - x)) * dt;
     var dy = (x * (b - z) - y) * dt;
@@ -53,8 +70,17 @@ function draw(){
     y = y + dy;
     z = z + dz;
     
+    var dx2 = (a * (y2 - x2)) * dt;
+    var dy2 = (x2 * (b - z2) - y2) * dt;
+    var dz2 = (x2 * y2 - c * z2) * dt;
+    
+    x2 = x2 + dx2;
+    y2 = y2 + dy2;
+    z2 = z2 + dz2;
+    
     //push new point to points array
     points.push([x, y, z])
+    points2.push([x2, y2, z2])
     it = it + 1;
   }
     
@@ -63,16 +89,21 @@ function draw(){
   //set origin to center of figure
   translate(width / 2, height / 2);
   
-  var hu = 0;
+  //var hu = 0;
   var result;
   for(var i = 0; i < points.length; i++){
-    fill(hu, 255, 255);
+    fill(h1, 200, 255);
     noStroke();
     result = math.multiply(points[i], rotmatrix).valueOf();
     ellipse(result[0] * delta, result[1] * delta, 5, 5);
-    hu = hu + 0.1
-    if(hu > 255)
-      hu = 0;
+    
+    fill(h2, 255, 255);
+    noStroke();
+    result = math.multiply(points2[i], rotmatrix).valueOf();
+    ellipse(result[0] * delta, result[1] * delta, 5, 5);
+    //hu = hu + 0.1
+    //if(hu > 255)
+      //hu = 0;
   }
 }
 
